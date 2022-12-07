003966e4ec220b57aed5988b4d21037ecbf3c45c
podopodo <phuntsho.gayenden@gmail.com>
2022-12-07 19:20:33 +0600

    corrected variable name

--- user/singleregister.php
+++ user/singleregister.php
@@ -55,7 +55,7 @@
   //$capacity = (int)$eventdetail[0]["capacity"];
   $total_registered = (int)json_decode(get("registration_requests","COUNT(id) as num","event_id=$eventid"),true)[0]["num"];
   //$accessingfrom=get_country();
-  $acessingfrom="Bhutan";
+  $accessingfrom="Bhutan";
   $regid = json_decode(get("registration_requests","id","cid='".$cid."' AND event_id='$eventid'"),true);
   if ($total_registered>=$capacity) {
     $generated_form = '<form id="msform">
