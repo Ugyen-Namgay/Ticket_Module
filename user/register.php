dd9fafb024ab9762216300c66806c16d2b6e08a7
podopodo <phuntsho.gayenden@gmail.com>
2022-12-11 23:13:58 +0600

    Layout of form

--- user/register.php
+++ user/register.php
@@ -770,7 +770,7 @@ $("#check_before_submit").click(function(){
           left = ((1-now) * 50)+"%";
           //3. increase opacity of previous_fs to 1 as it moves in
           opacity = 1 - now;
-          current_fs.css({'left': left});
+          current_fs.css({'left': left,'position': 'unset'});
           previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
         }, 
         duration: 800, 
